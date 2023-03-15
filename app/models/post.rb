133faaff1960ef5fa94be4ecd83b691fbc75ac8f
Cheong <xxlsxx1955@gmail.com>
2023-03-15 21:38:00 +0800

    Include comment body validation
    Sidenote:
    - Add some new line in post.rb and user.rb
      for own preference.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+
   validates :link, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :posts
+
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
 end
